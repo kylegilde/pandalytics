3ab0248c24e18ccd0b4a08dd6a7d546910814800
kylegilde <kylegilde@gmail.com>
2023-07-09 11:58:37 -0500

    improved test

--- tests/test_notebook_utils.py
+++ tests/test_notebook_utils.py
@@ -1,7 +1,8 @@
 from pandalytics.notebook_utils import create_code_toggle
+import IPython
 
 
 def test_create_code_toggle():
-    assert (
-        str(create_code_toggle()) == "<IPython.core.display.HTML object>"
-    ), "create_code_toggle did NOT return an <class 'IPython.core.display.HTML'> object"
+    assert isinstance(
+        create_code_toggle(), IPython.core.display.HTML
+    ), "create_code_toggle did NOT return a <class 'IPython.core.display.HTML'> object"
