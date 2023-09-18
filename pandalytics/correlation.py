81c36d62b67c974e607f648144901f1bddd2cbe0
kylegilde <kylegilde@gmail.com>
2023-09-18 09:41:08 -0500

    implemented y_col and tests

--- pandalytics/correlation.py
+++ pandalytics/correlation.py
@@ -1,75 +1,141 @@
-from typing import Optional, Literal
+from typing import Optional, Literal, Union
 from dataclasses import dataclass
+
 import numpy as np
 from numpy.typing import ArrayLike
 import pandas as pd
+
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 @dataclass
 class PairwiseCorrelations:
-    """ """
+    """
 
+    Parameters
+    ----------
+    method:
+    sep:
+    dtypes:
+
+    Returns
+    -------
+
+    """
+
+    y_col: Optional[Union[int, float, str]] = None
     method: Optional[Literal["pearson", "kendall", "spearman"]] = "spearman"
+    dtypes: Optional[ArrayLike] = ("number", "bool", "datetime", "datetimetz")
     sep: Optional[str] = " & "
-    dtypes: Optional[ArrayLike] = ("number", bool, "datetime64")
 
-    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
-        # df_corr_matrix = df.corr(self.method, numeric_only=True)
-        df_corr_matrix = df.select_dtypes(self.dtypes).corr(self.method)
-        nan_mask = np.triu(np.ones(df_corr_matrix.shape)).astype(bool)
-
-        self.df_pairwise_corr = (
-            df_corr_matrix.mask(nan_mask)  # sets the upper triangle to nans
-            .rename_axis("variable_1")
-            .reset_index()
-            .melt("variable_1", var_name="variable_2")
-            .dropna()
-            .assign(
-                abs_value=lambda df: df.value.abs(),
-                variable_pair=lambda df: df.variable_1.astype(str).str.cat(
-                    df.variable_2.astype(str), sep=self.sep
-                ),
-                is_positive=lambda df: df.value.ge(0),
-                text=lambda df: df.value.round(2),
-                method=self.method,
-            )
-            .sort_values("abs_value", ignore_index=True)
+    def _coersive_corr(self, s1, s2):
+        return (
+            pd.to_numeric(s1, errors="ignore")
+            .astype(float)
+            .corr(pd.to_numeric(s2, errors="ignore").astype(float), method=self.method)
         )
 
-        return self.df_pairwise_corr
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        Parameters
+        ----------
+        df
+
+        Returns
+        -------
+
+        """
+        if self.y_col:
+            # Calculate the correlation between 1 column and the rest
+            self.df_corr = (
+                df.drop(columns=self.y_col)
+                .select_dtypes(self.dtypes)
+                .apply(self._coersive_corr, s2=df[self.y_col])
+                .rename("value")
+                .rename_axis("y_label")
+                .reset_index()
+            )
+        else:
+            # Create the correlation matrix using the specified dtypes
+            df_corr_matrix = df.select_dtypes(self.dtypes).corr(self.method)
+            # Create a boolean mask for the upper triangle, including the diagonal
+            nan_mask = np.triu(np.ones(df_corr_matrix.shape, dtype=bool))
+
+            self.df_corr = (
+                df_corr_matrix.mask(nan_mask)  # sets the upper triangle to nans
+                .rename_axis("variable_1")
+                .reset_index()
+                .melt("variable_1", var_name="variable_2")
+                .dropna()  # drops the upper triangle of nans
+                .assign(
+                    y_label=lambda _df: _df.variable_1.astype(str).str.cat(
+                        _df.variable_2.astype(str), sep=self.sep
+                    )
+                )
+            )
+
+        # Create some metadata for the plot
+        self.df_corr = self.df_corr.assign(
+            abs_value=lambda _df: _df.value.abs(),
+            is_positive=lambda _df: _df.value.ge(0),
+            text=lambda _df: _df.value.round(2),
+            method=self.method,
+        ).sort_values("abs_value", ignore_index=True)
+
+        return self.df_corr
 
     def plot(
-        self,
-        df: pd.DataFrame,
-        min_abs_correlation: Optional[float] = None,
-        **kwargs,
-    ):
-        if not hasattr(self, "df_pairwise_corr"):
+            self,
+            df: pd.DataFrame,
+            min_abs_correlation: Optional[float] = None,
+            **kwargs,
+    ) -> go.Figure:
+        """
+
+        Parameters
+        ----------
+        df
+        min_abs_correlation
+        kwargs
+
+        Returns
+        -------
+
+        """
+        if not hasattr(self, "df_corr"):
             self.transform(df)
 
+        corr_type = (
+            f"{self.y_col} Correlations" if self.y_col else "Pairwise Correlations"
+        )
+
         if min_abs_correlation:
-            max_abs_value: float = self.df_pairwise_corr.abs_value.max()
+            max_abs_value: float = self.df_corr.abs_value.max()
             if min_abs_correlation > max_abs_value:
                 raise ValueError(
                     f"{min_abs_correlation = } is greater than "
                     f"maximum absolute value of {max_abs_value}."
                 )
 
-            df_plot = self.df_pairwise_corr.loc[
-                lambda df: df.abs_value.ge(min_abs_correlation)
+            df_plot = self.df_corr.loc[
+                lambda _df: _df.abs_value.ge(min_abs_correlation)
             ]
 
-            title = f"Showing {df_plot.shape[0]} of {self.df_pairwise_corr.shape[0]:,} Pairwise Correlations"
+            title = (
+                f"Showing {df_plot.shape[0]} of {self.df_corr.shape[0]:,}"
+                f" {corr_type}"
+            )
         else:
-            df_plot = self.df_pairwise_corr
-            title = f"{df_plot.shape[0]} Pairwise Correlations"
+            df_plot = self.df_corr
+            title = f"{df_plot.shape[0]} {corr_type}"
 
         return (
             px.bar(
                 df_plot,
                 x="abs_value",
-                y="variable_pair",
+                y="y_label",
                 color="value",
                 # pattern_shape="method",
                 text="text",
--- tests/test_correlation.py
+++ tests/test_correlation.py
@@ -1,5 +1,4 @@
-import numpy as np
-import pandas as pd
+import pytest
 import plotly.graph_objects as go
 from pandalytics.correlation import PairwiseCorrelations
 
@@ -8,9 +7,17 @@ def test_transform(df_pytest):
     pass
 
 
-def test_plot(df_pytest):
-    pc = PairwiseCorrelations()
-
+@pytest.mark.parametrize(
+    "y_col,method,dtypes,sep",
+    [
+        (None, "pearson", ["datetime"], " and "),
+        ("bool_col", "kendall", ["number", "bool", "datetime", "datetimetz"], None),
+        ("date_col", "spearman", ["bool"], None),
+        ("float_col", "kendall", ["number", "bool", "datetime", "datetimetz"], " - "),
+        (None, "kendall", ["number", "bool", "datetime", "datetimetz"], " - "),
+    ],
+)
+def test_plot(df_pytest, y_col, method, dtypes, sep):
+    pc = PairwiseCorrelations(y_col, method, dtypes, sep)
     fig = pc.plot(df_pytest)
-
     assert isinstance(fig, go.Figure), "The plot method did NOT return a go.Figure."
