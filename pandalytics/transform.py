955c1b5dff7456af86077d649c09f0066604e93e
Kyle Gilde <kylegilde@gmail.com>
2024-02-02 13:02:34 -0600

    fixed bug

--- pandalytics/transform.py
+++ pandalytics/transform.py
@@ -88,7 +88,7 @@ def flatten_column_names(df: pd.DataFrame, sep: Optional[str] = "_"):
     """
 
     df.columns = (
-        df.columns.to_flat_index().to_series().pipe(lambda s: s.name + "_" + s).values
+        df.columns.to_flat_index().to_series().pipe(lambda s: s.name + sep + s).values
     )
 
     return df
